050017d6c0e2dad28141aa99c97dbd63084565c9
CHRYSTAL BANKS <Chrystal.Banks1@gmail.com>
2015-11-29 17:55:03 -0500

    finished

--- js/spotify.js
+++ js/spotify.js
@@ -12,8 +12,31 @@ function searchByTrack(keyword) {
 }
 
 $("#search").submit(function(evt){
-  var keyword = $("#search-keyword").val();
-  console.log(keyword);
+  $("#results").empty();
   evt.preventDefault();
+  var keyword = $("#search-keyword").val();
+  var type = $("#search-type").val();
+  var url = 'http://ws.spotify.com/search/1/'+type+'.json?q=' + keyword;
+
+  $.ajax({
+    url: url,
+    type: 'GET',
+    dataType: 'json'
+  })
+  .done(function(response) {
+    var selectionProperty = type + 's';
+    var selection = response[selectionProperty];
+    console.log(selection);
+    $(selection).each(function(index, el) {
+      $("#results").append('<li><a href="'+ el.href+'">'+el.name+'</a></li>');
+    });
+  })
+  .fail(function() {
+    console.log("error");
+  })
+  .always(function() {
+    console.log("complete");
+  });
+
 
 });
